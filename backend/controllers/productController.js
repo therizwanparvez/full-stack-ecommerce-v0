14d0591a342e11fd028d931e16d32b8cfd6a3206
therizwanparvez <anqparvez@gmail.com>
2024-09-06 23:51:57 +0600

    [CRUD] Update a Product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/productModel");
 
+// Create Product
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
 
@@ -10,11 +11,35 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
+// Get All Product
 exports.getAllProduct = async (req, res, next) => {
   const products = await Product.find();
 
   res.status(200).json({
     success: true,
     products,
+  });
+};
+
+// Update a Product by Id
+exports.updateProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Product updated successfully",
   });
 };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -2,12 +2,13 @@ const express = require("express");
 const {
   createProduct,
   getAllProduct,
+  updateProduct,
 } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/product/new").post(createProduct);
-
+router.route("/product/:id").put(updateProduct);
 router.route("/products").get(getAllProduct);
 
 module.exports = router;
