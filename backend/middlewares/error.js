cee49b4f93253ce4048302aed5ae6f4fa5267d85
therizwanparvez <anqparvez@gmail.com>
2024-09-07 01:25:42 +0600

    Handle MongoDB Short ObejctId error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,6 +1,14 @@
+const ErrorHandler = require("../utils/errorHandler");
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.message = err.message || "Internal server error";
+
+  // MongoDB Short ObjectId Error
+  if (err.name === "CastError") {
+    const message = `Resouce not found. Invalid ${err.path}`;
+    err = new ErrorHandler(400, message);
+  }
 
   res.status(err.statusCode).json({
     success: false,
