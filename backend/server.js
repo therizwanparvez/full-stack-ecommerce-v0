c44b1312c3cc953796688da84b5848316eb158d2
therizwanparvez <anqparvez@gmail.com>
2024-09-06 22:25:57 +0600

    Create .env file for environment variables

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ const dotenv = require("dotenv");
 
 const app = require("./app");
 
-const port = 5000;
+dotenv.config();
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
