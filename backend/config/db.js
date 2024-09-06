eb5ed0399c4b93ad663dbc7154be6cc02b35bd4d
therizwanparvez <anqparvez@gmail.com>
2024-09-07 01:17:39 +0600

    Handle Unhandled Promise Rejection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDB = () => {
-  mongoose
-    .connect(process.env.MONGO_URI)
-    .then((data) => {
-      console.log(`MongoDB connected to: ${data.connection.host}`);
-    })
-    .catch((err) => {
-      console.log(`MongoDB connection error: ${err}`);
-    });
+  mongoose.connect(process.env.MONGO_URI).then((data) => {
+    console.log(`MongoDB connected to: ${data.connection.host}`);
+  });
 };
 
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -11,8 +11,16 @@ connectDB();
 
 const port = process.env.PORT || 5000;
 
-
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on: http://localhost:${port}`);
 });
 
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Error: ${err.messge}`);
+  console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+
+  server.close(() => {
+    process.exit(1);
+  });
+});
